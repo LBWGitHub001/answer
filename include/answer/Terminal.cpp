f7b3da0a713bac4c345ad3704ff402b54304dba5
LBWGitHub001 <13032652089@163.com>
2024-02-20 13:28:48 +0800

    Second commit

--- include/answer/Terminal.cpp
+++ include/answer/Terminal.cpp
@@ -26,28 +26,67 @@ void Terminal::callback(sensor_msgs::msg::Image msg) {
     cv::Mat grayImage;
     cv::cvtColor(image,grayImage,cv::COLOR_BGR2GRAY);
 
-    cv::imwrite("grayImage.png", grayImage);
+    //cv::imwrite("grayImage.png", grayImage);
     cv::Mat binImage;
-    cv::threshold(grayImage,binImage,200,255,cv::THRESH_BINARY);
-    cv::imwrite("binImage.png", binImage);
-
-    cv::Mat getLine = binImage.clone();
+    cv::threshold(grayImage,binImage,100,255,cv::THRESH_BINARY);
+    //cv::imwrite("binImage.png", binImage);
     std::vector<cv::Vec4f> lines; // 存储检测到的直线
-    HoughLinesP(getLine, lines, 1, CV_PI / 180, 600,500,10); // 应用霍夫直线检测算法
-    cv::Mat Lines = cv::Mat::zeros(getLine.rows,getLine.cols,CV_8UC3);
+/*
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(3,3));
+    cv::morphologyEx(binImage,binImage,cv::MORPH_CLOSE,kernel);
+    cv::imwrite("BIN.bng",binImage);*/
+    //检测click和判定线
+    cv::Mat corrider;
+    cv::Canny(binImage,corrider,50,150);
+    cv::imwrite("Canny.png",corrider);
+
+    HoughLinesP(binImage, lines, 1, CV_PI / 180, 100,60,3); // 应用霍夫直线检测算法
+    //save("image",binImage);
+    //Lines是图片lines是vector
+    cv::Mat judgeLine = image.clone();
+    cv::Mat click = image.clone();
     for (size_t i = 0; i < lines.size(); i++)
     {
         cv::Vec4f l = lines[i];
-        line(Lines, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]),
-             cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
-    }
-    //LineSet.push(Lines);                //向队列中添加
-    save("Lines",Lines);
+        cv::Scalar color;
+        cv::Point p1(l[0], l[1]);
+        cv::Point p2(l[2], l[3]);
+        int distance = (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y);
+        int yu = 160000;
+        if(distance > yu){
+            color = cv::Scalar(255,0,0);//蓝
+            line(judgeLine, p1, p2,color, 4,8);//, cv::LINE_AA
+        }
+        else{
+            color = cv::Scalar(0,0,255);//红
+            line(click, p1, p2,color, 2,4);//, cv::LINE_AA
+        }
 
+    }
+
+    save("JudgeLine",judgeLine);
+    save("dClick",click);
+    /*
+    //LineSet.push(Lines);                //向队列中添加
+
+    cv::Mat grayLine;
+    cv::cvtColor(Lines,grayLine,CV_RGB2GRAY);
+    cv::threshold(grayLine,grayLine,20,255,cv::THRESH_BINARY);
+    //cv::imwrite("grayLine.png",grayLine);
+
+    cv::bitwise_xor(grayLine,binImage,binImage);
+    //cv::imwrite("binImageE.png", binImage);
     cv::Mat grayClick;
     cv::threshold(click,grayClick,200,255,cv::THRESH_BINARY);
+    cv::Mat resultClick;
+    //cv::matchTemplate(binImage,grayClick,resultClick,CV_TM_SQDIFF);
+    cv::Point point;
+    cv::minMaxLoc(resultClick,0,0,&point);
+    line(binImage, cv::Point(point.x, point.y), cv::Point(point.x+click.cols,point.y),
+         cv::Scalar(0, 255, 0), 2, cv::LINE_AA);
+    cv::imwrite("binImageF.png", binImage);
+    */
 
-    count++;
 }
 
 void Terminal::save(std::string name, cv::Mat image) {
@@ -55,6 +94,9 @@ void Terminal::save(std::string name, cv::Mat image) {
     XorName += std::to_string(count);
     XorName += ".png";
     cv::imwrite(XorName, image);
+    count++;
+    //geometry_msgs::msg::Point32 msg;msg.x=count;msg.y=count;
+    //Publisher_->publish(msg);
 }
 
 void Terminal::LineProcess() {
